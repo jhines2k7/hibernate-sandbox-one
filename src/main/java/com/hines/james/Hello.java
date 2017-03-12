4364ed21299bd78a65c0f5c1aedcc4d190eecc80
James Hines <jameshines10@gmail.com>
2017-03-12 17:30:16 -0400

    Adds a few more interesting files

---
+++ src/main/java/com/hines/james/Hello.java
@@ -0,0 +1,7 @@
+package org.hines.james;
+
+/**
+ * Created by hinej4 on 10/31/2016.
+ */
+public class Hello {
+}
---
+++ src/main/java/com/hines/james/PlayerRepository.java
@@ -0,0 +1,9 @@
+package org.hines.james;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RestResource(path="players", rel="player")
+public interface PlayerRepository extends CrudRepository<Player,Long>{
+
+}
